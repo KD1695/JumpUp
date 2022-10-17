afe981f16071216eaa2662fddc928425043c69db
KD <kirtidhwajsingh95@gmail.com>
2022-10-17 13:40:04 -0600

    Add jump boost powerUp

---
+++ Assets/Scripts/PowerUps/JumpBoost.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpBoost : PowerUp
+{
+    public override void Activate()
+    {
+        PlayerState playerState = new PlayerState();
+        playerState.jumpForceMultiplier = (Random.Range(0,2)==0) ? 2 : -2;
+        playerState.playerHealth = 0;
+        UpdatePlayerState(playerState);
+    }
+
+    void Update()
+    {
+        
+    }
+}
