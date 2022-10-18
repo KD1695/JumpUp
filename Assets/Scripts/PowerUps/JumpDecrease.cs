d4a78ae46c42bde2afd9ce2b93e37f9f10ec145f
KD <kirtidhwajsingh95@gmail.com>
2022-10-17 20:39:48 -0600

    added jump decrease powerup

--- Assets/Scripts/PowerUps/JumpDecrease.cs
+++ Assets/Scripts/PowerUps/JumpDecrease.cs
@@ -2,20 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JumpBoost : PowerUp
+public class JumpDecrease : PowerUp
 {
     public override void Activate()
     {
         PlayClip();
         PlayerState playerState = new PlayerState();
-        playerState.jumpForceMultiplier = 2;
+        playerState.jumpForceMultiplier = -2;
         playerState.playerHealth = 0;
         playerState.resetTimer = Duration;
         UpdatePlayerState(playerState);
-    }
-
-    void Update()
-    {
-        
     }
 }
